46c5c1c34b112531b37b8d41dc9cac3681be461b
theseventhshadow <ang.millan@duocuc.cl>
2025-09-28 17:28:04 -0300

    Titulo

--- src/main/kotlin/Libro.kt
+++ src/main/kotlin/Libro.kt
@@ -1,2 +1,5 @@
 package org.example
 
+fun libro(){
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 package org.example
 
 fun main() {
-    println("Sistema de Gestión de Bibliotecas.")
+    println("Sistema de Gestión de Bibliotecas BookSmart.")
 }
